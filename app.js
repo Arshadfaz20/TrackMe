2d1d04b9578a84956bff5f0989b474d0cd0e0ac1
Arshadfaz20 <mfazeer@deakin.edu.au>
2020-07-26 08:25:51 +1000

    ENHANCE: javascript for files

--- app.js
+++ app.js
@@ -1,25 +1,4 @@
-const devices = [];
-devices.push({ user: "Mary", name: "Mary's iPhone" });
-devices.push({ user: "Alex", name: "Alex's Surface Pro" });
-devices.push({ user: "Mary", name: "Mary's MacBook" });
-
-devices.forEach(function(device) {
-    const table = document.querySelector('#devices');
-    const row = document.createElement('tr');
-
-    const user = document.createElement('td');
-    const userText = document.createTextNode(device.user);
-    user.appendChild(userText);
-
-    const name = document.createElement('td');
-    const nameText = document.createTextNode(device.name);
-    name.appendChild(nameText);
-
-    row.appendChild(user);
-    row.appendChild(name);
-
-    table.appendChild(row);
-   });
+const devices = JSON.parse(localStorage.getItem('devices')) || [];
 
    devices.forEach(function(device) {
     $('#devices tbody').append(`
@@ -28,6 +7,18 @@ devices.forEach(function(device) {
     <td>${device.name}</td>
     </tr>`
     );
+   });
+
+   $('#add-device').on('click', function() {
+    const user = $('#user').val();
+    const name = $('#name').val();
+    devices.push({ user, name });
+    location.href = 'device-list.html';
+   });
+   
+   $('#send-command').on('click', function() {
+    const command = $('#command').val();
+    console.log(`command is: ${command}`);
    });
    
    
