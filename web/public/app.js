9492887d4f11ede3608056ed3f763a25a1c1b4e7
Arshadfaz20 <mfazeer@deakin.edu.au>
2020-07-27 23:26:19 +1000

    ENHANCE: Added features for reg an dlogin

--- web/public/app.js
+++ web/public/app.js
@@ -2,6 +2,8 @@ $('#navbar').load('navbar.html');
 $('#footer').load('footer.html');
 
 const devices = JSON.parse(localStorage.getItem('devices')) || [];
+const users = JSON.parse(localStorage.getItem('users')) || [];
+
 
    devices.forEach(function(device) {
     $('#devices tbody').append(`
@@ -12,6 +14,8 @@ const devices = JSON.parse(localStorage.getItem('devices')) || [];
     );
    });
 
+
+
    $('#add-device').on('click', function() {
     const user = $('#user').val();
     const name = $('#name').val();
@@ -21,10 +25,46 @@ const devices = JSON.parse(localStorage.getItem('devices')) || [];
     location.href = '/';
 
    });
+ 
+ 
+   $('#login').on('click', () => {
+      const username = $('#user').val();
+      const password = $('#password').val();
+      const users = JSON.parse(localStorage.getItem('users')) || [];
+      const exists = users.find(user => user.name === username);
+      if (exists && exists.password === password) {
+        localStorage.setItem('isAuthenticated', true);
+        location.href = '/';
+      } else {
+        $('#message').append('<p class="alert alert-danger">Authentication failed</p>');
+      }
+    });
    
+     $('#register').on('click', () => {
+      const username = $('#user').val();
+      const password = $('#password').val();
+      const confirm = $('#confirm').val();
+      const users = JSON.parse(localStorage.getItem('users')) || [];
+      const exists = users.find(user => user.name === username);
+      if (exists) {
+        $('#message').append('<p class="alert alert-danger">User already exists</p>');
+      } else if (password !== confirm) {
+        $('#message').append('<p class="alert alert-danger">Passwords do not match</p>');
+      } else {
+        users.push({ name: username, password });
+        localStorage.setItem('users', JSON.stringify(users));
+        location.href = '/login';
+      }
+    });
+
+    const logout = () => {
+      localStorage.removeItem('isAuthenticated');
+      localStorage.removeItem('user');
+      location.href = '/login';
+  }
+
    $('#send-command').on('click', function() {
     const command = $('#command').val();
     console.log(`command is: ${command}`);
    });
-   
    
